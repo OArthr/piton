0cfb9cd97a6471871f2b3018d08af5e99603ca48
Brenno Siqueira <140553743+BrennoDS@users.noreply.github.com>
2024-10-25 14:44:31 -0300

    icone

--- Dream/Dream_Codding/main.py
+++ Dream/Dream_Codding/main.py
@@ -7,6 +7,8 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('Dream') 
+        pygame_icon = pygame.image.load('Dream/Dream_Codding/grafico/Icon.png.png')
+        pygame.display.set_icon(pygame_icon)
         self.clock = pygame.time.Clock()
         self.level = Level()
 
@@ -22,6 +24,7 @@ class Game:
             self.level.run(dt)
             pygame.display.update()
 
+#Verifica se está no main
 if __name__ == '__main__':
     game = Game()
     game.run()

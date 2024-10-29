c574974bff276a95ec71f36d94e7418a42edc859
Brenno Siqueira <140553743+BrennoDS@users.noreply.github.com>
2024-10-29 11:10:05 -0300

    redirecionamento

--- Dream/Dream_Codding/Mapa/tlies/fences.tsx
+++ Dream/Dream_Codding/Mapa/tlies/fences.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="fences" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../../../../../Documentos/GitHub/piton/Dream/Dream_Codding/Mapa/sprites/fences.png" width="64" height="64"/>
+ <image source="../../../../../../../Área de Trabalho/piton/Dream/Dream_Codding/Mapa/sprites/fences.png" width="64" height="64"/>
 </tileset>

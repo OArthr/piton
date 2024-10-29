8239653c4d591693e0a60cdc5fdec06531663773
Brenno Siqueira <140553743+BrennoDS@users.noreply.github.com>
2024-10-29 11:06:32 -0300

    reorganização dos tiles

--- Dream/Dream_Codding/Mapa/tlies/Collision.tsx
+++ Dream/Dream_Codding/Mapa/tlies/Collision.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Collision" tilewidth="16" tileheight="16" tilecount="64" columns="16">
- <image source="../../../../../../../../Downloads/s10 - Collisions/s10 - Collisions/graphics/environment/Collision.png" width="256" height="64"/>
+ <image source="../sprites/Collision.png" width="256" height="64"/>
 </tileset>
--- Dream/Dream_Codding/Mapa/tlies/Objetos.tsx
+++ Dream/Dream_Codding/Mapa/tlies/Objetos.tsx
@@ -2,12 +2,12 @@
 <tileset version="1.10" tiledversion="1.11.0" name="Objetos" tilewidth="48" tileheight="48" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../Sprout Lands - Sprites - Basic pack/Objects/Free_Chicken_House.png" width="48" height="48"/>
+  <image source="../sprites/Free_Chicken_House.png" width="48" height="48"/>
  </tile>
  <tile id="1">
-  <image source="../Sprout Lands - Sprites - Basic pack/Objects/Egg_item.png" width="16" height="16"/>
+  <image source="../sprites/Egg_item.png" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image source="../Sprout Lands - Sprites - Basic pack/Objects/arvore.png" width="22" height="29"/>
+  <image source="../sprites/arvore.png" width="24" height="31"/>
  </tile>
 </tileset>

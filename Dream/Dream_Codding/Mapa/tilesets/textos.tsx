35beaadfe08846ae8c60948e61956419514ddd79
BrennoSFd <diasbsf@gmail.com>
2024-11-18 18:56:40 -0300

    Update

--- Dream/Dream_Codding/Mapa/tilesets/textos.tsx
+++ Dream/Dream_Codding/Mapa/tilesets/textos.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="textos" tilewidth="150" tileheight="150" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="textos" tilewidth="150" tileheight="150" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
  <tile id="4">
@@ -13,5 +13,23 @@
  </tile>
  <tile id="7">
   <image source="questoes/questao4.png" width="150" height="150"/>
+ </tile>
+ <tile id="9">
+  <image source="questoes/dica1.png" width="150" height="150"/>
+ </tile>
+ <tile id="10">
+  <image source="questoes/dica2.png" width="150" height="150"/>
+ </tile>
+ <tile id="11">
+  <image source="questoes/dica3.png" width="150" height="150"/>
+ </tile>
+ <tile id="12">
+  <image source="questoes/dica41.png" width="150" height="150"/>
+ </tile>
+ <tile id="13">
+  <image source="questoes/dica42.png" width="150" height="150"/>
+ </tile>
+ <tile id="16">
+  <image source="questoes/dica0.png" width="150" height="150"/>
  </tile>
 </tileset>
